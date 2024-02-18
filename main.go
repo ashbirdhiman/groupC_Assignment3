96b0402bbbcb9a3ff905ff27973521812f27808d
ashbirdhiman <38061870+ashbirdhiman@users.noreply.github.com>
2024-02-17 23:03:46 -0500

    Creating main.go
    *has import required for assignment
    *created struct required for assignment
    * added DPORT to using 8012 port number

---
+++ main.go
@@ -0,0 +1,24 @@
+package main
+
+import (
+	// "encoding/json"
+	"fmt"
+	// "net/http"
+	// "strings"
+
+	// "github.com/google/uuid"
+)
+
+
+type Item struct {
+    ID   string `json:"id"`
+    Name string `json:"name"`
+}
+
+// var items = []Item{} // In-memory storage for items
+const Dport = ":8012"
+
+func main() {
+	fmt.Println("Group C Assignment 3");
+}
+
